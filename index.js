0009eb62872a7ee318fea0d1575b9e70f41dea71
Marko Kazimirovic <55856557+yepistream@users.noreply.github.com>
2024-10-03 20:24:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+//Import style for overlay
+import '../styles/OverlayElementStyle.css';
+
 // Import the SimpleIFrame and SmartFloatingPanel classes from SimpleElement.js
-import { SimpleIFrame, SmartFloatingPanel } from './SimpleElement.js';
+import { HTMLOverlay, HTMLOverlay3D } from './SimpleElement.js';
 
 // Export the SimpleIFrame class and its subsidiary classes
-export { SimpleIFrame, SmartFloatingPanel };
+export { HTMLOverlay, HTMLOverlay3D };
